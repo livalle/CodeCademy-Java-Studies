b4157c7a0019a84923463800dc7cd7ef61d58ada
Lidia Valle <lidiavale88@gmail.com>
2024-05-22 12:39:57 -0300

    Create Doc.java
    Good practices and basics in Java

---
+++ Doc.java
@@ -0,0 +1,25 @@
+/* Good practices and basics in Java:
+int, which stores whole numbers.
+double, which stores bigger whole numbers and decimal numbers.
+boolean, which stores true and false.
+char, which stores single characters using single quotes.
+String, which stores multiple characters using double quotes.
+Static typing, which is one of the safety features of Java.
+Variable naming conventions.*/
+
+public class MyProfile {
+	public static void main(String[] args) {   
+String name = "Lidia"; /*Strings must have clear and comprehensive names.
+    It also, should start with only letters,$ or _ ,
+    and using camelCase for good practice and good quality of code. */
+    
+ int age = 23; // int stores only whole numbers, nothing more.
+ 
+  double desiredSalary = 4.000; //In other hand, double is used for storing decimal numbers like : 3.4  22.3 etc...
+    
+   char gender = 'F'; /*char is a single character holder that must always be followed with a '' 
+   differently from String it only holds a single character and nothing else */
+    
+    boolean lookingForJob = true;  //decision makers that helps or program take "yes" or "No" decisions
+	}
+}
